a09d1f64f669293c248da5f99b04fcc1f792a2f7
Horus <gjg000@Mac-M2.local>
2025-04-25 20:29:37 +0200

    Commit

--- zonestealer.py
+++ zonestealer.py
@@ -7,7 +7,6 @@ import argparse
 
 NS = dr.Resolver()
 
-# Target domain
 Domain = 'inlanefreight.com'
 
 NS.nameservers = ['ns1.inlanefreight.com', 'ns2.inlanefreight.com']
@@ -32,17 +31,31 @@ def AXFR(domain, nameserver):
                 pass
 
 if __name__=="__main__":
+    parser = argparse.ArgumentParser(prog="dns-axfr.py", epilog="DNS Zonetransfer Script", usage="dns-axfr.py [options] -d <DOMAIN>", prefix_chars='-', add_help=True)
+    parser.add_argument('-d', action='store', metavar='Domain', type=str, help='Target Domain.\tExample: inlanefreight.htb', required=True)
+    parser.add_argument('-n', action='store', metavar='Nameserver', type=str, help='Nameservers separated by a comma.\tExample: ns1.inlanefreight.htb,ns2.inlanefreight.htb')
 
-        for nameserver in NS.nameservers:
+    args = parser.parse_args()
+    if not args.d:
+        print('[!] You must specify target Domain.\n')
+        print(parser.print_help())
+        exit()
 
-                AXFR(Domain, nameserver)
+    if not args.n:
+        print('[!] You must specify target nameservers.\n')
+        print(parser.print_help())
+        exit()
 
+        # Variables
+    Domain = args.d
+    NS.nameservers = list(args.n.split(","))
+
+    for nameserver in NS.nameservers:
+        AXFR(Domain, nameserver)
         if Subdomains is not None:
                 print('-------- Found Subdomains:')
-
                 for subdomain in Subdomains:
                         print('{}'.format(subdomain))
-
         else:
                 print('No subdomains found.')
                 exit()         
