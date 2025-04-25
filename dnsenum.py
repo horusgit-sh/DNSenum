6e7ddf0c2054c26640f9198e763e7e8edb1888bc
Horus <gjg000@Mac-M2.local>
2025-04-25 19:30:42 +0200

    Commit

--- dnsenum.py
+++ dnsenum.py
@@ -1,47 +1,60 @@
+#!/usr/bin/env python3
+
+# Dependencies:
+# python3-dnspython
+
+# Used Modules:
 import dns.zone as dz
 import dns.query as dq
 import dns.resolver as dr
 import argparse
 
+# Initialize Resolver-Class from dns.resolver as "NS"
 NS = dr.Resolver()
 
+# Target domain
 Domain = 'inlanefreight.com'
 
+# Set the nameservers that will be used
 NS.nameservers = ['ns1.inlanefreight.com', 'ns2.inlanefreight.com']
 
-
+Subdomains = []
 
 def AXFR(domain, nameserver):
-    list = []
-    try:
-        axfr = dz.from_xfr(dq.xfr(nameserver, domain))
 
-        if axfr:
-            print('[*] Successful Zone Transfer from {}'.format(nameserver))
-            for record in axfr:
-                list.append('{}.{}'.format(record.to_text(), domain))
-        return list
+        try:
+                axfr = dz.from_xfr(dq.xfr(nameserver, domain))
 
-    except Exception as error:
-        print(error)
-        pass
+                if axfr:
+                        print('[*] Successful Zone Transfer from {}'.format(nameserver))
 
+                        for record in axfr:
+                            sub = '{}.{}'.format(record.to_text(), domain)
+                            if sub not in Subdomains:
+                                Subdomains.append(sub)
 
+        # If zone transfer fails
+        except Exception as error:
+                print(error)
+                pass
 
-if __name__ == "__main__":
-    for nameserver in NS.nameservers:
-        Subdomains = AXFR(Domain, nameserver)
+# Main
+if __name__=="__main__":
 
-    if Subdomains is not None:
-        print('-------- Found Subdomains:')
+        # For each nameserver
+        for nameserver in NS.nameservers:
 
-        for subdomain in Subdomains:
-            print('{}'.format(subdomain))
+                #Try AXFR
+                AXFR(Domain, nameserver)
 
-    else:
-        print('No subdomains found.')
-        exit()
+        # Print the results
+        if Subdomains is not None:
+                print('-------- Found Subdomains:')
 
+                # Print each subdomain
+                for subdomain in Subdomains:
+                        print('{}'.format(subdomain))
 
-
-
+        else:
+                print('No subdomains found.')
+                exit()         
